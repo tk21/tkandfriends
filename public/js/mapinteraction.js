3e72ffe3861b188d8951fdaf7e3c4c62124cd665
David C <davidxcanas@gmail.com>
2015-06-02 02:19:45 -0700

    Fixed the things
    all was broken. Put stuff in the right place and changed paths

---
+++ public/js/mapinteraction.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $('#mappie').mapster({
+    singleSelect: false,
+    fill: true,
+    altImage: "../img/ColoredRegionsAll.jpg",
+    fillOpacity: 1,
+  });
+});
+
+$('#regionList li').click(function () {
+  var region = $(this).attr('id').replace('List', '');
+  region = '#' + region;
+  //This is where the the background color is set
+  //TODO make this toggle
+  $(this).css("background-color", "skyblue");
+  //Modular means of setting regions via list
+  //THIS ALREADY TOGGLES
+  $(region).mapster('set');
+});
