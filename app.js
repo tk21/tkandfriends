843eaaf334e7f5ff28ffa79a75e794f172c810a9
tk21 <teresado.21t@gmail.com>
2015-06-01 21:00:07 -0700

    stuff

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.load();
 //connect to database
 var conString = process.env.DATABASE_CONNECTION_URL;
 
-//Configures the Template engine
+//configures the Template engine
 app.engine('handlebars', handlebars({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
@@ -32,21 +32,54 @@ app.use(session({ secret: 'keyboard cat',
 app.set('port', process.env.PORT || 3000);
 
 //routes
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
   console.log("index should be rendered.")
 });
 
+//render bar chart
+app.get('/bars', function (req, res) {
+  res.render('bars');
+  console.log(req.query);
+  console.log("Attempted to render bar chart.");
+});
+
+//retrieve data 
 app.get('/delphidata', function (req, res) {
-  console.log("reaching app get delphi data");
+  console.log("----------------------- APPJS: DELPHIDATA------------------");
+  console.log("THIS IS ONLY FILTERS OF QUERY:");
+  console.log(req.query.f);
+
+  var db = req.query.categories;
+  var filters = req.query.f;
+  var education = "hhsa_san_diego_demographics_education_2012_norm";
+  var industry = "hhsa_san_diego_demographics_occupat_indust_2012_norm";
+  var mar_status = "hhsa_san_diego_demographics_marital_status_2012_norm";
+
+
+  //SELECT.....
+  var query = "SELECT * FROM ";
+
+
+  //FROM....... (database)
+  query += db.split(" ")[0] + " ";
+
+
+  //WHERE....... (filters)
+  query += "WHERE \"" + category + "\"=\'" + 
+
 
   // initialize connection pool 
   pg.connect(conString, function(err, client, done) {
     if(err) return console.log(err);
     
+    //console.log("DELPHI DATA-----------------------------------------------------\n");
+    //console.log("Retrieving: hhsa_san_diego_demographics_occupat_industry_2012_norm");
+    /**
     var query = 'SELECT * FROM hhsa_san_diego_demographics_occupat_industry_2012_norm';
-    console.log("DELPHI DATA-----------------------------------------------------\n");
-    console.log("Retrieving: hhsa_san_diego_demographics_household_composition_2012");
+    //query += '  WHERE \"Industry\"=\'Utilities\'';
+    console.log("this is your query:");
+    console.log(query);
 
     client.query(query, function(err, result) {
       // return the client to the connection pool for other requests to reuse
@@ -54,8 +87,8 @@ app.get('/delphidata', function (req, res) {
 
       res.writeHead("200", {'content-type': 'application/json'});
       res.end(JSON.stringify(result.rows));
-    });
-  });
+    });**/
+  }); 
 });
 
 http.createServer(app).listen(app.get('port'), function() {
--- public/js/index.js
+++ public/js/index.js
@@ -12,16 +12,23 @@ var DelphiDemo = DelphiDemo || (function() {
   //     $("#delphi-table").append(rows);
   //   });
 
+  // Reads in user input on forms to build the query string to be sent to database
   self.buildQuery = function() {
     console.log("INDEXJS: building query with strings----------------");
 
-    var query = "SELECT * ";
+    var q = "SELECT * ";
 
-    var occupat_indust = " FROM hhsa_san_diego_demographics_occupat_industry_2012_norm";
-    var education = " FROM hhsa_san_diego_demographics_education_2012_norm";
-    var mar_status = " FROM hhsa_san_diego_demographics_marital_status_2012_norm";
+    var occupat_indust = "FROM hhsa_san_diego_demographics_occupat_industry_2012_norm";
+    var education = "FROM hhsa_san_diego_demographics_education_2012_norm";
+    var mar_status = "FROM hhsa_san_diego_demographics_marital_status_2012_norm";
+
+
+    //testing
+    q += occupat_indust;
+
 
     var filters = "";
+    var category = "Industry";
 
     // concatenate string that has all the filters user selected
     console.log("these are the values you checked off:");
@@ -35,20 +42,70 @@ var DelphiDemo = DelphiDemo || (function() {
 
 
     if (filters != "") {
-      query += "WHERE Industry=" + filters;
+      q += "WHERE \"" + category + "\"=\'" + filters + "\'";
     }
 
-
     console.log("this is now query:");
-    console.log(query);
+    console.log(q);
 
     return "query finished building";
   };
 
+  // Takes query string and passes it into the database to be processed
   self.getDelphiData = function() {
     console.log("INDEXJS: getting delphi data------------------");
 
-    $.getJSON("/delphidata", function(data) {
+    var filters = "";
+    var cats = "";
+
+    var checked_education = false;
+    var checked_industry = false;
+    var checked_mar_status = false;
+
+    //pull from EDUCATION database if checked 
+    $('input[name="education"]').each(function() {
+      if ($(this).is(":checked")) {
+        checked_education = true;
+      }
+    });
+    if (checked_education) {
+      cats += "Education" + " ";
+    }
+
+    //pull from INDUSTRY database if checked 
+    $('input[name="industry"]').each(function() {
+      if ($(this).is(":checked")) {
+        checked_industry = true;
+      }
+    });
+    if (checked_industry) {
+      cats += "Industry" + " ";
+    }
+
+    //pull from MARITAL STATUS database if checked 
+    $('input[name="marital_status"]').each(function() {
+      if ($(this).is(":checked")) {
+        checked_mar_status = true;
+      }
+    });
+    if (checked_mar_status) {
+      cats += "Mar_status" + " ";
+    }
+
+    //concatenate user-selected filters
+    console.log("these are the values you checked off:");
+    $("input[type=checkbox]:checked").map(function() {
+      console.log(this.value);
+      filters += this.value + " ";
+    });
+
+    //send AJAX GET request to get delphi data with chosen filters
+    $.ajax({
+      url: "/bars",
+      data: {f: filters, categories: cats}
+    });
+
+    /**$.getJSON("/delphidata", function(data) {
       console.log("DATA INSIDE:");
       console.log(data);
 
@@ -58,14 +115,11 @@ var DelphiDemo = DelphiDemo || (function() {
     //   });
 
     //   return data;
-    });
+    }); **/
   };
 
   // initialize
   self.init = function() {
-    var query;
-    query = self.buildQuery();
-
     self.getDelphiData();
   };
   return self;
