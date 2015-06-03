8fa3579c98f4b7f01e4e8303b2fe59cd434e5de7
David C <davidxcanas@gmail.com>
2015-06-03 03:31:54 -0700

    Added list, need to make hover work and vis button appear/dissapear

--- public/js/mapinteraction.js
+++ public/js/mapinteraction.js
@@ -15,13 +15,17 @@ $(document).ready(function () {
       console.log(region)
       var listColor = $('#' + region).css("background-color");
       console.log(listColor);
-      if($('#' + region).hasClass('active')){
+      if($('#' + region).hasClass('active_region')){
         console.log('here');
-        $('#' + region).removeClass('active');
+        $('#' + region).removeClass('active_region');
       }else{
-        $('#' + region).addClass('active');
+        $('#' + region).addClass('active_region');
       }
     }
+  });
+  
+  $('#region_list_button').click(function(){
+    $('#region_li').slideToggle();
   });
   
   $('#regionList li').click(function () {
@@ -29,16 +33,23 @@ $(document).ready(function () {
     var region = $(this).attr('id').replace('List', '');
     region = '#' + region;
     //This is where the the background color is set
-    if($(this).hasClass('active')){
-      $(this).removeClass('active');
+    if($(this).hasClass('active_region')){
+      $(this).removeClass('active_region');
     }else{
-      $(this).addClass('active');
+      $(this).addClass('active_region');
     }
     //Modular means of setting regions via list
     //THIS ALREADY TOGGLES
     $(region).mapster('set');
   });
   
-  
-
+//  NEED TO FIGURE OUT HOW TO MAKE MAP HIGHLIGHT ON LIST HOVER  
+//  $('#regionList li').mouseenter(function () {
+//    var region = $(this).attr('id').replace('List', '');
+//    region = '#' + region;
+//    $(region).mapster('highlight');
+//    $('this').mouseleave(function () {
+//      $(region).mapster('highlight',false);
+//    });
+//  });
 });
