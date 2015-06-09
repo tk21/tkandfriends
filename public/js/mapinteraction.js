c86afe6bf85df70e5b179db5f1c2e6f544bdf832
tk21 <teresado.21t@gmail.com>
2015-06-09 13:31:18 -0700

    oops forgot to add the introjs files and libs

--- public/js/mapinteraction.js
+++ public/js/mapinteraction.js
@@ -5,58 +5,57 @@ $(document).ready(function () {
     $('#region_li').slideToggle();
   });
   
-  $('#Education_Button, #Industry_Button, #Marital_Status_Button').click(function(){
-    
+  //ON DB BUTTON CLICK....
+  $('#Education_Button, #Industry_Button, #Marital_Status_Button').click(function(){ 
+    console.log("we clicked a button");
     var db_color = $(this).css('background-color');
-    
+    console.log(db_color);
+
     $('.body').css('background-color', db_color);
-    
+
+    console.log(db_color);
     db_color = rgb2hex(db_color);
-  
+
     $('#region_list_area').show();
     $('#help_button').hide();
-    
+        
+    //activates mapster after DB button click only
     $('#map').mapster({
-    singleSelect: false,
-    fill: true,
-    //altImage: "../img/ColoredRegionsAll.jpg",
-    fillColor: db_color,
-    fillOpacity: .8,
-    mapKey : 'id',
-    scaleMap:true,
-    wrapClass:'pull-right',
-    onClick: function (e) {
-      var region = 'li[region_in_list="' + e.key + ' List"]';
-      if($(region).hasClass('active_region')){
-        //why?!
-        console.log('here');
-        $(region).removeClass('active_region');
-      }else{
-        $(region).addClass('active_region');
+      singleSelect: false,
+      fill: true,
+      //altImage: "../img/ColoredRegionsAll.jpg",
+      fillColor: db_color,
+      fillOpacity: .8,
+      mapKey : 'id',
+      scaleMap:true,
+      wrapClass:'pull-right',
+      onClick: function (e) {
+        var region = 'li[region_in_list="' + e.key + ' List"]';
+        if($(region).hasClass('active_region')){
+          //why?!
+          console.log('here');
+          $(region).removeClass('active_region');
+        }else{
+          $(region).addClass('active_region');
+        }
       }
+  });
+
+  $('#Education_Button, #Industry_Button, #Marital_Status_Button').each(function(){
+    var button = $(this).attr('id');
+
+    $('#region_li li').removeClass('active_region');
+    if( $(this).attr('id') == button) {
+      $(this).attr('name', 'active');
+    }
+    else {
+      $(this).attr('name', '');
     }
   });
-    
-    
+});   //end of document ready
 
-    $('#Education_Button, #Industry_Button, #Marital_Status_Button').each(function(){
-      
-      var button = $(this).attr('id');
-      
-      $('#region_li li').removeClass('active_region');
-
-        if( $(this).attr('id') == button) {
-          $(this).attr('name', 'active');
-        }
-        else {
-          $(this).attr('name', '');
-        }
-    });
-  });
-  
   
   //list->map
-  
   $('#region_li li').click(function () {
     var area_toggle;
     
@@ -80,10 +79,20 @@ $(document).ready(function () {
   
   //Converts RGB to Hex Values
   function rgb2hex(rgb) {
+    console.log("inside rgb to hex");
+
+    //rgb gets fucked up after INTROJS, this is a bandaid.
+    if ( (rgb.match(/,/g) || []).length == 3){
+      console.log("we gotta remov esome things");
+      console.log(rgb.substring(0, rgb.lastIndexOf(',')) + ")");
+      rgb = rgb.substring(0, rgb.lastIndexOf(',')) + ")";
+    }
+
     rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
     function hex(x) {
         return ("0" + parseInt(x).toString(16)).slice(-2);
     }
+
     return hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
   }
 
